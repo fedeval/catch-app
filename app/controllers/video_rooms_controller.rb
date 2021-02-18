21cc27ebe2ecdea9d9b7397da4a0641b23f380fc
Federico Valenza <federico.valenza5@gmail.com>
2021-02-18 20:20:35 +0100

    fixed naming inconsistencies

--- app/controllers/video_rooms_controller.rb
+++ app/controllers/video_rooms_controller.rb
@@ -2,7 +2,7 @@ class VideoRoomsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @room = Room.find(params[:id])
+    @room = VideoRoom.find(params[:id])
     opentok = OpenTok::OpenTok.new ENV['VONAGE_API_KEY'], ENV['VONAGE_API_SECRET']
     @token = opentok.generate_token @room.session_id
   end
@@ -13,13 +13,11 @@ class VideoRoomsController < ApplicationController
     opentok = OpenTok::OpenTok.new ENV['VONAGE_API_KEY'], ENV['VONAGE_API_SECRET']
     session = opentok.create_session
     
-    @room = Room.new(name: 'Test', session_id: session.session_id)
+    @room = VideoRoom.new(name: 'Test', session_id: session.session_id)
     if @room.save
-      format.html { redirect_to @room, notice: "Room was successfully created." }
-      format.json { render :show, status: :created, location: @room }
+      render :show
     else
-      format.html { render :new, status: :unprocessable_entity }
-      format.json { render json: @room.errors, status: :unprocessable_entity }
+      render root_path
     end
   end
 
