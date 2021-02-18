0b314f6daa45e0866e8204c731e759e8b4d44efe
Leonardo Dacoreggio <dacoreggio@gmail.com>
2021-02-18 21:16:52 +0100

    removed the gem merit and its files, added migration with new joint table for user_badges

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  has_merit
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_202221) do
+ActiveRecord::Schema.define(version: 2021_02_18_202222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,15 @@ ActiveRecord::Schema.define(version: 2021_02_18_202221) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "user_badges", force: :cascade do |t|
+    t.bigint "badge_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["badge_id"], name: "index_user_badges_on_badge_id"
+    t.index ["user_id"], name: "index_user_badges_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -110,6 +119,8 @@ ActiveRecord::Schema.define(version: 2021_02_18_202221) do
     t.index ["second_match"], name: "index_video_sessions_on_second_match"
   end
 
+  add_foreign_key "user_badges", "badges"
+  add_foreign_key "user_badges", "users"
   add_foreign_key "video_sessions", "users", column: "first_match"
   add_foreign_key "video_sessions", "users", column: "second_match"
 end
