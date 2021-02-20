a3a074fbd581aed214dbee1c24fb3f420ed6cbdd
susann kaechler <sanni51@gmx.de>
2021-02-20 14:10:18 +0100

    changed auhorization

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,7 @@
 class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
-    authorize @chatroom
     @message = Message.new
+    authorize @chatroom
   end
 end
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,3 +1,9 @@
 class Chatroom < ApplicationRecord
   has_many :messages
+  validates :maximum_users_per_chatroom
+
+  private
+  def maximum_users_per_chatroom
+    user.count <= 2
+  end
 end
