44514151d5c1520517d695daf0844945922b094e
Federico Valenza <federico.valenza5@gmail.com>
2021-02-21 11:59:03 +0100

    added model validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,6 @@
 class Booking < ApplicationRecord
+  validates :start_time, presence: true, numericality: { greater_than: Time.now }
+
+  belongs_to :user_one, class_name: 'User'
+  belongs_to :user_two, class_name: 'User'
 end
