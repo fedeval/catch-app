82484438dbb9b999895c41b8e4f4818f9d820f58
Federico Valenza <federico.valenza5@gmail.com>
2021-02-21 12:17:29 +0100

    fix bookings validations after model update

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,6 @@
 class Booking < ApplicationRecord
-  validates :start_time, presence: true, numericality: { greater_than: Time.now }
+  validates :start_hour, presence: true, numericality: { in: (0..24) }
+  validates :start_minutes, presence: true, numericality: { in: (0...60) }
 
   belongs_to :user_one, class_name: 'User'
   belongs_to :user_two, class_name: 'User'
