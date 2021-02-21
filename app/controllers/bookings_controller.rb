c677b0b6fc6ccd641416a9c3fdf7820bc06f70f1
Federico Valenza <federico.valenza5@gmail.com>
2021-02-21 15:06:16 +0100

    add update and create actions to booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,11 +5,31 @@ class BookingsController < ApplicationController
     @bookings = policy_scope(Booking)
   end
 
-  def create; end
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user_one = current_user
+    authorize(@booking)
+    if @booking.save
+      redirect_to 'dashboard/index'
+    else
+      render 'bookings/index'
+    end
+  end
 
-  def update; end
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.user_two = current_user
+    authorize(@booking)
+    if @booking.save
+      redirect_to 'dashboard/index'
+    else
+      render 'bookings/index'
+    end
+  end
 
   private
 
-  def booking_params; end
+  def booking_params
+    params.require(:booking).permit(:start_hour, :start_minutes)
+  end
 end
--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -4,4 +4,8 @@ class BookingPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def update?
+    true
+  end
 end
