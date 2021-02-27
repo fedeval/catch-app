61c35fb779ab8f201af358e453f1132f89a8c453
Leonardo Dacoreggio <dacoreggio@gmail.com>
2021-02-27 16:07:57 +0100

    cleaning gst

--- app/controllers/user_badges_controller.rb
+++ app/controllers/user_badges_controller.rb
@@ -11,6 +11,14 @@ class UserBadgesController < ApplicationController
     authorize @user_badge
   end
 
+  def assign(category)
+    @badge = Badge.find_by(category: category)
+    UserBadge.create(
+      @receiver_id = @user_two
+      @sender_id = current_user
+      @badge_id = @badge.id);
+  end
+
   def index
     @badges = policy_scope(UserBadge)
   end
