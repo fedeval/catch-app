4ed63551894d001615c83476f7590e53d7132450
Leonardo Dacoreggio <dacoreggio@gmail.com>
2021-02-27 15:18:40 +0100

    new view to assign badges. JS file initiated, not concluded yet. Route to index is /user_badges

--- app/controllers/user_badges_controller.rb
+++ app/controllers/user_badges_controller.rb
@@ -1,14 +1,25 @@
 class UserBadgesController < ApplicationController
+  before_action :authenticate_user!
 
   def new
     @user_badge = UserBadge.new
+    authorize @user_badge
   end
 
-  # def create
-    #@user_badge = UserBadge.new(badge_params)
-#   end
+  def create
+    @user_badge = UserBadge.new(receiver_id, badge)
+    authorize @user_badge
+  end
+
+  def index
+    @badges = policy_scope(UserBadge)
+  end
 
 # def badge_params
 #     params.require(:badge).permit(:)
 #   end
+
+  private
+    @receiver_id = "@video_room_receiver_id"
+
 end
