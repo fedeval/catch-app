68287fc413ac76ac1ac88f7c06d017eb0a8af01a
juanpao <juanpao@gmail.com>
2021-03-02 20:35:37 +0100

    Add dotenv - Protect my secret data in .env file

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+CLOUDINARY_URL=cloudinary://329756767345378:VWm0Myy_xKht2zqBJVXAWSQxBQw@dywmzkxwf
+
+VONAGE_API_KEY=47132654
+
+VONAGE_API_SECRET=a59c2534bee3248561aca414fe73716b4fa9950e
