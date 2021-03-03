c6ca68f7470f96b169ef233ca0eda560f23c02a1
Federico Valenza <federico.valenza5@gmail.com>
2021-03-03 22:12:43 +0100

    style fixes

--- app/javascript/animations/show_timepicker.js
+++ app/javascript/animations/show_timepicker.js
@@ -6,14 +6,14 @@ const showTimepicker = () => {
     form.classList.remove('hidden');
     const instantMatches = document.querySelector('.instant-matches');
     instantMatches.classList.add('hidden');
-    const instantMatchesArrow = document.getElementById('im-arrow-up');
+    const instantMatchesArrow = document.getElementById('im-arrow-down');
     instantMatchesArrow.classList.remove('hidden')
     timepickerArrow.classList.add('hidden')
   });
 }
 
 const hideTimepicker = () => {
-  const instantMatchesArrow = document.getElementById('im-arrow-up');
+  const instantMatchesArrow = document.getElementById('im-arrow-down');
 
   instantMatchesArrow.addEventListener("click", function() {
     const form = document.getElementById('timepicker');
