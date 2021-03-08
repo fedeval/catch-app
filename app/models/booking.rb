8c38aee69a5e074c833bdd69522893e55deda202
Federico Valenza <federico.valenza5@gmail.com>
2021-03-08 16:01:46 +0100

    add class method to store times that can only be confirmed via instant match into an array

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,4 +5,14 @@ class Booking < ApplicationRecord
 
   belongs_to :user_one, class_name: 'User'
   belongs_to :user_two, class_name: 'User', optional: true
+
+  def self.unavailable_times
+    bookings = Booking.all.where(user_two: nil)
+    unavailable_times = []
+    bookings.each do |booking|
+      nine_min_from_start_time = booking.start_time + (60 * 9)
+      unavailable_times << [booking.start_time.strftime('%H:%M'), nine_min_from_start_time.strftime('%H:%M')]
+    end
+    unavailable_times
+  end
 end
