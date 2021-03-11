cd27799724c57acd3355859dab61e36944b7653f
juanpao <juanpao@gmail.com>
2021-03-11 20:03:01 +0100

    intro with logo plus bigger type

--- app/javascript/packs/delay_js.js
+++ app/javascript/packs/delay_js.js
@@ -2,7 +2,7 @@
 const delay = document.getElementsByClassName("enter__text")[0];
 delay.addEventListener("click", (event) => {
 
-  setTimeout( function() { window.location = '/bookings' }, 2000 );
+  setTimeout( function() { window.location = '/bookings' }, 2500 );
 });
 
 // window.onload = delay;
