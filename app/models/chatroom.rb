ba201589335f1d6b72fa6cdcbb48a81d217b87da
Federico Valenza <federico.valenza5@gmail.com>
2021-03-12 17:19:49 +0100

    footer background transparent

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,5 +1,5 @@
 class Chatroom < ApplicationRecord
   has_many :messages, dependent: :destroy
-  belongs_to :friendship, class_name: "HasFriendship::Friendship"
+  belongs_to :friendship, class_name: "HasFriendship::Friendship", dependent: :destroy
   validates :friendship, uniqueness: true
 end
