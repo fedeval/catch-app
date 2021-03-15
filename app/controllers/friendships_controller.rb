5717bfbadcd1cb058625d6065d48ebb691a9bb8f
fedeval <federico.valenza5@gmail.com>
2021-03-15 21:13:03 +0100

    remove unnecessary comments and minor formatting edits

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,4 @@
 class FriendshipsController < ApplicationController
-
   def add_friend
     booking = Booking.find(params[:booking_id])
     other_user = booking.user_one == current_user ? booking.user_two : booking.user_one
@@ -20,19 +19,4 @@ class FriendshipsController < ApplicationController
     authorize(contact)
     redirect_to dashboard_index_path(view: "contacts")
   end
-
-  # def on_friendship_accepted(friendship)
-
-  # end
-
-  # def on_friendship_blocked(friendship)
-
-  # end
-
-  # def on_friendship_destroyed(friendship)
-
-  # end
 end
-
-
-# https://github.com/sungwoncho/has_friendship  >> all functionalities
